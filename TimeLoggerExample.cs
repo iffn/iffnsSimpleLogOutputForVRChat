a65765fdf691578dbcb9e336e381e584e38791fb
iffn <iffn1990@gmail.com>
2022-05-06 13:16:31 +0200

    Initial commit

---
+++ TimeLoggerExample.cs
@@ -0,0 +1,60 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using System.Diagnostics;
+using iffnsStuff.iffnsVRCStuff.DebugOutput;
+
+namespace iffnsStuff.iffnsVRCStuff.DebugOutput.Examples
+{
+    public class TimeLoggerExample : UdonSharpBehaviour
+    {
+        [SerializeField] iffnsSimpleLogOutput logOutput;
+
+        System.DateTime offsetDateTime;
+
+        Stopwatch stopwatchSinceStart;
+
+        double referenceTime;
+
+        void Start()
+        {
+            referenceTime = Networking.GetServerTimeInSeconds();
+
+            offsetDateTime = System.DateTime.UtcNow;
+
+            stopwatchSinceStart = new Stopwatch();
+
+            stopwatchSinceStart.Start();
+
+            logOutput.SetTitle(
+                "Stopwatch time" + logOutput.GetTabSymbol() +
+                "Computer time" + logOutput.GetTabSymbol() +
+                "Unity time" + logOutput.GetTabSymbol() +
+                "VRC Server time" + logOutput.GetTabSymbol() +
+                "My time"
+                );
+        }
+
+        private void Update()
+        {
+            if (Input.GetKey(KeyCode.P))
+            {
+                double stopwatchTime = stopwatchSinceStart.Elapsed.TotalSeconds;
+                double ComputerTime = (System.DateTime.UtcNow - offsetDateTime).TotalSeconds;
+                float UnityTime = Time.time;
+                double VRCServerTime = Networking.GetServerTimeInSeconds();
+                double myTime = referenceTime + stopwatchSinceStart.Elapsed.TotalSeconds;
+
+                logOutput.AddInput(
+                    stopwatchTime.ToString("F6") + logOutput.GetTabSymbol() +
+                    ComputerTime.ToString("F6") + logOutput.GetTabSymbol() +
+                    UnityTime.ToString("F6") + logOutput.GetTabSymbol() +
+                    VRCServerTime.ToString("F6") + logOutput.GetTabSymbol() +
+                    myTime.ToString("F6")
+                    );
+            }
+        }
+    }
+}
---
+++ iffnsSimpleLogOutput.cs
@@ -0,0 +1,50 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using UnityEngine.UI;
+
+namespace iffnsStuff.iffnsVRCStuff.DebugOutput
+{
+    public class iffnsSimpleLogOutput : UdonSharpBehaviour
+    {
+        [SerializeField] InputField field;
+        [SerializeField] bool EnableDeleteKey = true;
+
+        const string newLine = "\n";
+
+        string title;
+
+        public void SetTitle(string title)
+        {
+            this.title = title;
+            field.text = title + newLine;
+        }
+
+        public void AddInput(string input)
+        {
+            field.text += input + newLine;
+        }
+
+        public string GetTabSymbol()
+        {
+            return "\t";
+        }
+
+        private void Update()
+        {
+            /*
+            if (Input.GetKey(KeyCode.P))
+            {
+                Logger.AddInput("" + Time.time + "\t" + Time.deltaTime);
+            }
+            */
+
+            if (EnableDeleteKey && Input.GetKey(KeyCode.Delete))
+            {
+                field.text = title + newLine;
+            }
+        }
+    }
+}
